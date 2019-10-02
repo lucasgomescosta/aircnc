5bce704a19905a4e3770f45e2c650fa24434cea1
lucas <lucas.gomes@ufrr.br>
2019-10-02 16:32:03 -0400

    backend
    semana omnistack rocketseat

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,13 @@
+const Spot = require('../models/Spot');
+
+
+module.exports = {
+    
+    async store(req, res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        
+
+        return res.json({ok: true })
+    }
+};
---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const pe = require('parse-error');
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-gjnco.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = }Acessar route params (para edição, delete)
+//req.body = Acessar corpo da requisição(para criação, edição)
+
+
+app.use(routes);
+
+app.listen(3333);
+
+process.on('unhandledRejection', error => {
+    console.error('Uncaught Error', pe(error));
+});
