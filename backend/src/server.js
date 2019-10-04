7e58078b1ca344744a0fd5610dcef45e0bee2841
lucas <lucas.gomes@ufrr.br>
2019-10-04 17:26:00 -0400

    FrontEnd
    construindo a interface web

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
-const pe = require('parse-error');
+const cors = require('cors');
+const path = require('path');
+
+//const pe = require('parse-error');
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-gjnco.mongodb.net/semana09?retryWrites=true&w=majority',{
@@ -15,11 +18,15 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-gjnco.mongodb.net/
 //req.params = }Acessar route params (para edição, delete)
 //req.body = Acessar corpo da requisição(para criação, edição)
 
+app.use(cors())
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
 
+/*
 process.on('unhandledRejection', error => {
     console.error('Uncaught Error', pe(error));
 });
+*/
